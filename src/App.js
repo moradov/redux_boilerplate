13e7762a832a8d70682a1681e4e9d8df467cbaa2
moradov <moradazyzy@gmail.com>
2021-03-08 11:55:04 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './store';
 import Home from './Home';
-import { PostsContextProvider } from './context/PostsContext/postContext';
+import AddPost from './components/AddPost';
+import EditPost from './components/EditPost';
+import Navbar from './components/UI/Navbar';
+import Alert from './components/UI/Alert';
 
-const App = () => {
+export default function App() {
   return (
-    <PostsContextProvider>
-      <Home />
-    </PostsContextProvider>
+    <Router>
+      <Provider store={store}>
+        <Navbar />
+        <Alert />
+        <Switch>
+          <Route path="/add">
+            <AddPost />
+          </Route>
+          <Route path="/edit/:postId">
+            <EditPost />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Provider>
+    </Router>
   );
-};
-
-export default App;
+}
